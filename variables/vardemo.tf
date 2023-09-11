147232b68624b415789eaeba1186df0f40ab6747
renanrcrr <renanrcrr.developer@gmail.com>
2023-09-11 12:54:18 +1000

    Added variable concepts

---
+++ variables/vardemo.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "var_demo" {
+  name = "renanlabs"
+}
+
+ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cird_blocks = [var.vpn_ip]
+}
+
+ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cird_blocks = [var.vpn_ip]
+}
+
+ingress {
+    from_port = 53
+    to_port = 53
+    protocol = "tcp"
+    cird_blocks = [var.vpn_ip]
+}
---
+++ variables/variables.tf
@@ -0,0 +1,3 @@
+variable "vpn_ip" {
+  default = "117.50.30.20/32"
+}

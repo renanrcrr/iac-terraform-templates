f20adadd70710a2019e467efe6bcae19025cfe5a
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-18 20:26:20 +0930

    Update region and ec2 instance.

--- first-ec2/my-first-ec2.tf
+++ first-ec2/my-first-ec2.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  region     = "ap-southeast-2" # Region: Sydney
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
 }
 
-resource "aws_instance" "my-first-ec2" {
-  ami = "ami-0373aa64534d82bf6"
-  instance_type = "t2.micro" 
+resource "aws_instance" "myec2" {
+  ami = "ami-02346a771f34de8ac"
+  instance_type = "t2.micro"
 }

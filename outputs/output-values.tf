94cf5a8be90c377c6d5e6806318571f1e3e3c811
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-20 13:45:49 +0930

    Updating output variables.

--- outputs/output-values.tf
+++ outputs/output-values.tf
@@ -15,6 +15,17 @@ resource "aws_eip" "lb" {
 #     value = aws_eip.lb.public_ip
 # }
 
+# 1) Display a specific URL created
+output "public_ip" {
+    value = "https://${aws_eip.lb.public_ip}:8080"
+}
+
+# 2) Display the public ip address
+output "public_ip" {
+    value = aws_eip.lb.public_ip
+}
+
+# 3) Display the public ip object with all attributes associated to
 output "public_ip" {
     value = aws_eip.lb
 }

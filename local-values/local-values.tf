0183689c56edf5c8fc820e14178876638ab5ed7d
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-31 18:57:46 +0930

    Working with local values.

---
+++ local-values/local-values.tf
@@ -0,0 +1,31 @@
+# Locals vs Variables
+# Variable value can be defined in wide variety of places like terraform.tfvars, ENV variables, CLI and so on.
+# Locals are more of a private resources and have to directly modify the code.
+# Locals are used when I want to avoid repeating the same expression multiple times.
+
+
+variable "tags" {
+    type = map
+    default = {
+        Team = "security-team"
+    }
+}
+
+# Locals allow to add expressions, which allow to compute values dynamically
+locals {
+    default = {
+        Team = "security-team"
+        CreationDate = "date-${formatdate("DDMMYY", timestamp())}"
+    }
+}
+
+# Local values are created by a locals block (plural), but I reference them as attributes on an object named local (singular)
+resource "aws_security_group" "sg_01" {
+  name = "app_firewall"
+  tags = local.default
+}
+
+resource "aws_security_group" "sg_02" {
+  name = "db_firewall"
+  tags = local.default
+}

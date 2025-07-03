c430cd7a32d8fb207ed0ee9a9755f700c18a04cf
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-07-03 17:05:13 +0930

    Remove AWS provider configuration and versioning files

--- provider-config/aws_provider_config.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region     = "ap-southeast-2"
-    shared_credentials_files = ["/Users/Renam Rodrigues/.aws/credentials"]
-}
-
-resource "aws_iam_user" "demouser" {
-    name = "terraform-demo-user"
-}

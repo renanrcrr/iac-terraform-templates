16333e11d922e27c76c4613acc3fea46dd5ce8b1
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-07-07 04:30:00 +0930

    Remove deprecated variable and resource definitions for AWS configurations

--- variables/central-variables.tf
+++
@@ -1,7 +0,0 @@
-variable "vpn_ip" {
-  default = "101.0.62.210/32"
-}
-
-variable "app_port" {
-  default = "8080"
-}
--- variables/variable-definition-file/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "ami" {
-    description = "This AMI is for Sydney region"
-    default = "ami-123" # Terraform will use it only when none value was defined at file.tfvars
-}

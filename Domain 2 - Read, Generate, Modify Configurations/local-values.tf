413c9ce41cc482dcdd42ca6ffa1b62a019d22a3e
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-07-07 18:09:32 +0930

    Update locals.

--- Domain 2 - Read, Generate, Modify Configurations/local-values.tf
+++ Domain 2 - Read, Generate, Modify Configurations/local-values.tf
@@ -1,8 +1,9 @@
 # Locals vs Variables
-# Variable value can be defined in wide variety of places like terraform.tfvars, ENV variables, CLI and so on.
+#
+# Variable value can be defined in wide variety of places 
+# like terraform.tfvars, ENV variables, CLI and so on.
 # Locals are more of a private resources and have to directly modify the code.
 # Locals are used when I want to avoid repeating the same expression multiple times.
-
 
 variable "tags" {
     type = map

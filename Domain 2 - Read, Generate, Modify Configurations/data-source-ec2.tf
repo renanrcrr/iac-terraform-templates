05b2cbc41788a6c825498aeee8fe3da212583f81
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-07-08 03:50:27 +0930

    Add data sources examples and modify count index and functions.

---
+++ Domain 2 - Read, Generate, Modify Configurations/data-source-ec2.tf
@@ -0,0 +1,3 @@
+data "aws_instance" "example" {
+    
+}
--- Domain 2 - Read, Generate, Modify Configurations/functions.tf
+++ Domain 2 - Read, Generate, Modify Configurations/functions.tf
@@ -1,19 +1,19 @@
-# # Define an AWS IAM user resource named "this"
-# resource "aws_iam_user" "this" {
-#   # Specify the name of the IAM user
-#   name = "demo-rcrr-user"
-# }
+# Define an AWS IAM user resource named "this"
+resource "aws_iam_user" "this" {
+  # Specify the name of the IAM user
+  name = "demo-rcrr-user"
+}
 
-# # Define an AWS IAM user policy resource named "lb_ro"
-# resource "aws_iam_user_policy" "lb_ro" {
-#   # Specify the name of the IAM user policy
-#   name = "demo-user-policy"
+# Define an AWS IAM user policy resource named "lb_ro"
+resource "aws_iam_user_policy" "lb_ro" {
+  # Specify the name of the IAM user policy
+  name = "demo-user-policy"
   
-#   # Associate the policy with the IAM user created above
-#   # The user is referenced using the "aws_iam_user.this.name" attribute
-#   user = aws_iam_user.this.name
+  # Associate the policy with the IAM user created above
+  # The user is referenced using the "aws_iam_user.this.name" attribute
+  user = aws_iam_user.this.name
 
-#   # Load the policy document from an external JSON file
-#   # The "file" function reads the contents of the specified file
-#   policy = file("./iam-user-policy.json")
-# }
+  # Load the policy document from an external JSON file
+  # The "file" function reads the contents of the specified file
+  policy = file("./iam-user-policy.json")
+}
